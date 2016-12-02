722eb5f74143644941558043f0b8ac842d4ce020
Arvind Nagarajan <arvind.nagarajan@gmail.com>
2016-12-02 01:06:21 -0800

    Add Second View controller and Navigation Controller.

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var squareView: UIView!
+    @IBOutlet weak var textBox: UITextField!
     // This is a comment
     // This is another comment
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //self.updateBackgroundColor()
+        squareView.layer.borderColor = UIColor.black.cgColor
+        squareView.layer.borderWidth = 1
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,6 +31,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func updateBackgroundColor() {
+        /*
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value) */
+        /*view.backgroundColor = UIColor(colorLiteralRed: redSlider.value, green: greenSlider.value, blue: blueSlider.value, alpha: 1.0)*/
+        textBox.backgroundColor = UIColor(colorLiteralRed: redSlider.value, green: greenSlider.value, blue: blueSlider.value, alpha: 1.0)
+        squareView.backgroundColor = UIColor(colorLiteralRed: redSlider.value, green: greenSlider.value, blue: blueSlider.value, alpha: 1.0)
+    }
 }
 
